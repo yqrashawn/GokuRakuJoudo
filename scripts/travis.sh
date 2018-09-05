de554d2e7a15cfcbb3ee006f9d6b28c229fb46e6
yqrashawn <namy.19@gmail.com>
2018-09-05 22:29:44 +0800

    test travis cache

---
+++ scripts/travis.sh
@@ -0,0 +1,12 @@
+brew_watchexec="/usr/local/Cellar/watchexec/1.9.0/"
+brew_lein="/usr/local/Cellar/leiningen/2.8.1/"
+if [ -d "$brew_watchexec" ]; then
+    ln -s "$brew_watchexec"bin/watchexec /usr/local/bin
+else
+    brew install watchexec
+fi
+if [ -d "$brew_lein" ]; then
+    ln -s "$brew_lein"bin/lein /usr/local/bin
+else
+    brew install leiningen
+fi
