2bce5d76a18fa93d2fdaf7a86b09b93e59cc87df
yqrashawn <namy.19@gmail.com>
2020-12-25 15:39:27 +0800

    feat: upgrade clojure and graalvm version

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main karabiner-configurator.core
   :aot :all
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/tools.cli "0.4.2"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.cli "1.0.194"]
                  [me.raynes/fs "1.4.6"]
-                 [cheshire "5.8.0"]
-                 [environ "1.1.0"]]
+                 [cheshire "5.10.0"]
+                 [environ "1.2.0"]]
   :plugins [[lein-cloverage "1.0.13"]
             [lein-environ "1.1.0"]]
   :profiles {:dev {:env {:is-dev true}}
