ae5dd6fc7048d88240cbe253d4e992037111be19
Anton Korniychuk <11693557+korniychuk@users.noreply.github.com>
2022-04-25 12:47:07 +0300

    Add support of "dictation" key (#171)
    Co-authored-by: Muhammed Zakir <8190126+MuhammedZakir@users.noreply.github.com>

--- src/karabiner_configurator/core.clj
+++ src/karabiner_configurator/core.clj
@@ -211,7 +211,7 @@
       (:version options)
       {:action "exit-with-message"
        :ok? true
-       :exit-message "0.5.1"}
+       :exit-message "0.5.2"}
       ;; log
       (:log options)
       {:action       "log"
